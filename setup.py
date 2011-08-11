c211aa4c60a19ddccd38dd4341e0b443ca1192d8
Petr Viktorin <encukou@gmail.com>
2011-08-11 16:13:25 +0200

    Bare Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pokemwdb',
+    version='0.1',
+    author='En-Cu-Kou',
+    author_email='encukou@gmail.com',
+    install_requires=[
+    ],
+    packages=find_packages(exclude=['ez_setup']),
+)
